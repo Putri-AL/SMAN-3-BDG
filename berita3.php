d07404eca6b7bc6edc7a3df7ce4d8560eeed4da3
Putri-AL <putriayu2776@gmail.com>
2024-10-20 21:50:25 +0700

    Add files via upload

---
+++ berita3.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Mi Muhammadiyah 2 Bandung</title>
+    <link rel="stylesheet" href="CSS/style_projek.css">
+</head>
+
+<body>
+    <div class="kolom">
+        <div class="container">
+            <div class="email_mi">
+                <figure><img src="c:\PUTRI AYU LESTARI\Informatika\Website_XI4\Projek Putri Ayu\img\email-removebg-preview.png" height="50px" width="50px"></figure>
+                <h3>mim2bdg@gmail.com</h3>
+            </div>
+            <div class="telepon_mi">
+                <h3> +62 823-1226-9821</h3>
+                <figure><img src="c:\PUTRI AYU LESTARI\Informatika\Website_XI4\Projek Putri Ayu\img\telepon-removebg-preview.png" height="26px" width="30px"></figure>
+            </div>
+        </div>
+    </div>
+
+
+    <nav class="navbar">
+        <div class="container1">
+            <div class="navlogo">
+                <figure><img src="c:\PUTRI AYU LESTARI\Informatika\Website_XI4\Projek Putri Ayu\img\logo_sma_3-removebg-preview.png" height="95px" width="85px"></figure>
+                <h2 style="float: left; padding-top: 1.0em;">SMA Negeri 3 Bandung</h2>
+                <p style="float: left;" >Knowladge is Power but Character is More</p>
+            </div>
+            <div class="navitem">
+                <ul>
+                    <li><a href="berita_smanti.html">Berita Terkait</a></li>
+                    <li><a href="kontak_projek.html">Kontak</a></li>
+                    <li><a href="profil_projek.html">Profil</a></li>
+                    <li><a href="projek_putri.html">Beranda</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
